5d5be381432c526646b6837d2abc097308ef4e5f
Jakub <j.jedrzejek@gmail.com>
2019-10-29 16:37:19 +0100

    final commit

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log('działa');
+
+let size = 100;
+let grow = true;
+
+const square = document.createElement('div');
+document.body.appendChild(square);
+square.style.width = size + "px"
+square.style.height = size + "px"
+
+window.addEventListener('scroll', function () {
+
+    if (size >= window.innerWidth / 2) {
+        grow = !grow;
+    }
+
+    else if (size == 0) {
+        grow = true; // !grow ?
+    }
+
+
+    if (grow) {
+        size += 5;
+        square.style.width = size + "px";
+        square.style.height = size + "px";
+    }
+
+    else {
+        size -= 5;
+        square.style.width = size + "px";
+        square.style.height = size + "px";
+    }
+
+})
